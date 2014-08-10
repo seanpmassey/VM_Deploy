55931b1e2630e09cec2e6b8b30df2839cc4bb330
seanpmassey <seanpmassey@gmail.com>
2014-08-09 23:34:22 -0500

    Added New Process files

---
+++ Create-SQLInstanceDisks.ps1
@@ -0,0 +1,114 @@
+﻿Param($InstanceName,$DataDriveLetter = "R",$LogDriveLetter = "T", $DataVolumeSize = "20", $LogVolumeSize = "10",$Servername)
+
+Function Create-VMDK
+{
+	#Create VMDK File on Server and Return VMDK File Name
+	Param ($Servername,$VolumeSize,$Controller)
+	
+    $disk = New-HardDisk -VM $servername -Persistence persistent -CapacityGB $VolumeSize -StorageFormat Thin -Confirm:$false -Controller $Controller
+
+	$Filename = $disk.filename
+	$pos = $Filename.IndexOf("/")
+	$Filename = $Filename.Substring($pos+1)
+	
+	#$pos = $Filename.IndexOf(".")
+	#$Filename = $Filename.Substring(0,$pos)
+    
+    #Write-Output $Filename
+	
+	Return $Filename
+}
+
+Function Create-MountPoint
+{
+	#Initialize volume, create partition, mount as NTFS Mount Point, and format as NTFS Volume
+	Param($Servername,$VolumeName,$VolumeSize,$Path,$CimSession)
+		$VolumeSizeGB = [string]$VolumeSize + "GB"
+	
+	$partition = Get-Disk -CIMSession $CimSession | Where-Object {($_.partitionstyle -eq "raw") -and ($_.size -eq $VolumeSizeGB)} | Initialize-Disk -PartitionStyle GPT -PassThru |  New-Partition -UseMaximumSize -AssignDriveLetter:$False 
+	
+	$disknumber = $partition.DiskNumber
+	$partitionnumber = $partition.partitionnumber
+	$diskID = $partition.diskID
+
+	Get-Partition -DiskNumber $disknumber -PartitionNumber $partitionnumber -CimSession $CimSession | Format-Volume -AllocationUnitSize 64KB -FileSystem NTFS -NewFileSystemLabel $VolumeName -Confirm:$false 
+	Add-PartitionAccessPath -CimSession $CimSession -DiskNumber $disknumber -PartitionNumber $partitionnumber -AssignDriveLetter:$False
+	Add-PartitionAccessPath -CimSession $CimSession -DiskNumber $disknumber -PartitionNumber $partitionnumber -AccessPath $Path 
+	Set-Partition -CimSession $CimSession -DiskNumber $disknumber -PartitionNumber $partitionnumber -NoDefaultDriveLetter:$true
+}
+
+add-pssnapin vmware.vimautomation.core
+Import-Module ActiveDirectory
+
+#Configure vCenter Connection
+$vCenterUser = "Service Account UPN"
+$PWHash = Get-Content Location of Password Hash file
+$key = $key = Get-Content Location of Password Key File
+$securestring = ConvertTo-SecureString -String $PWHash -Key $key
+$vCenterCred = New-Object –TypeName System.Management.Automation.PSCredential –ArgumentList $vCenterUser, $securestring
+
+#Configure Remote Server Connection
+$ServerUser = "Service Account UPN"
+$PWHash = Get-Content Location of Password Hash file
+$key = $key = Get-Content Location of Password Key File
+$securestring = ConvertTo-SecureString -String $PWHash -Key $key
+$ServerCred = New-Object –TypeName System.Management.Automation.PSCredential –ArgumentList $Serveruser, $securestring
+
+Connect-VIServer vCenterFQDN -Credential $vCenterCred
+
+$CimSession = New-CimSession -ComputerName $Servername -Credential $ServerCred
+
+Try
+{
+	$Random = Get-Random -Minimum 1 -Maximum 2
+
+	Switch ($Random)
+	{
+		1
+			{
+			$DataController = "SCSI Controller 1"
+			$LogController = "SCSI Controller 2"
+			}
+		2
+			{
+			$DataController = "SCSI Controller 2"
+			$LogController = "SCSI Controller 1"
+			}
+	}
+
+	If($InstanceName -eq "Default")
+	{$InstanceName = "MSSQL"}
+
+	#Create Data Drive
+	$Filename = Create-VMDK -Servername $Servername -Volumesize $DataVolumeSize -Controller $DataController
+	$Path = $DataDriveLetter+":\$Instancename"
+	New-Item -ItemType Directory -Path "\\$Servername\$DataDriveLetter`$\$InstanceName"
+	$VolumeName = "Data-"+$InstanceName+"-"+$Filename
+	If($VolumeName.length -gt 32)
+	{
+		$Length = $Filename.Length
+		$TempFileName = $Filename.Substring(6,$Length)
+		$VolumeName = "Data-"+$TempInstanceName+"-"+$TempFilename
+	}
+	Create-Mountpoint -Servername $Servername -VolumeName $VolumeName -VolumeSize $DataVolumeSize -Path $Path -CIMSession $CimSession
+
+	#Create Log Volume
+	$Filename = Create-VMDK -Servername $Servername -Volumesize $LogVolumeSize -Controller $LogController
+	$Path = $LogDriveLetter+":\$Instancename"
+	New-Item -ItemType Directory -Path "\\$Servername\$LogDriveLetter`$\$InstanceName"
+	$VolumeName = "Logs-"+$InstanceName+"-"+$Filename
+	If($VolumeName.length -gt 32)
+	{
+		$Length = $Filename.Length
+		$TempFileName = $Filename.Substring(6,$Length)
+		$VolumeName = "Data-"+$TempInstanceName+"-"+$TempFilename
+	}
+	Create-Mountpoint -Servername $Servername -VolumeName $VolumeName -VolumeSize $LogVolumeSize -Path $Path -CIMSession $CimSession
+
+}
+Finally
+{
+	Remove-CIMSession $CimSession 
+	
+	Disconnect-VIServer -Confirm:$false
+}
---
+++ Provision-VM.ps1
@@ -0,0 +1,110 @@
+﻿Param($Servername,$location = "FTDC1",$IPAddress,[switch]$Dev,$CPUCount=1,$RAMCount=4,$Description,$Version="2012")
+Add-PSSnapin VMware.DeployAutomation
+
+
+add-pssnapin vmware.vimautomation.core
+Import-Module ActiveDirectory,SQLPS
+
+#Configure AD Credentials
+$ADUser = "Service Account UPN"
+$PWHash = Get-Content Location of Password Hash file
+$key = $key = Get-Content Location of Password Key File
+$securestring = ConvertTo-SecureString -String $PWHash -Key $key
+$ADCred = New-Object –TypeName System.Management.Automation.PSCredential –ArgumentList $ADUser,$securestring
+
+#Configure vCenter Connection
+$vCenterUser = "Service Account UPN"
+$PWHash = Get-Content Location of Password Hash file
+$key = $key = Get-Content Location of Password Key File
+$securestring = ConvertTo-SecureString -String $PWHash -Key $key
+$vCenterCred = New-Object –TypeName System.Management.Automation.PSCredential –ArgumentList $vCenterUser,$securestring
+
+Connect-VIServer vCenterFQDN -Credential $vCenterCred
+
+$LocationDetails = Invoke-Sqlcmd -query "Select * FROM OSCustomizationSettings WHERE Location_ID = '$Location'" -database OSCustomizationDB -serverinstance "SQLServer\Instance"
+
+$SubnetMask = $LocationDetails.Location_NetMask
+$DefaultGW = $LocationDetails.Location_GW
+$DataStore = $LocationDetails.Location_Datastore
+$ESXiHost = $LocationDetails.Location_Host
+$DNS = $LocationDetails.Location_DNS
+$DNS = $DNS.Split(",")
+
+Try{$TestAD = Get-ADComputer -Identity $Servername -ErrorAction SilentlyContinue}
+Catch{Write-Output "Computer Account does not exist."}
+
+Try
+{
+    $OSSpecName = "$Servername-NPSpec"
+    $TestCustSpec = Get-OSCustomizationSpec -Name $OSSpecName -ErrorAction SilentlyContinue}
+    Catch{Write-Output "Customization Spec already exists."
+}
+
+Try
+{
+	If($TestAD -ne $Null)
+	{
+		Write-Output "AD Account Already Exists."
+	}
+	Else
+	{
+		If($Dev -ne $true)
+		{
+			New-ADComputer -Name $Servername -Path "OU Distinguished Name" -Server DCFQDN -Credential $ADCred
+		}
+		Else
+		{
+			New-ADComputer -Name $Servername -Path "OU Distinguished Name" -Server DCFQDN -Credential $ADCred
+		}
+	}
+	
+
+	If($TestCustSpec -ne $null)
+	{
+		Write-Output "Customization Spec Already Exists"
+	}
+	Else
+	{
+		Get-OSCustomizationSpec -Name AutomationSpec | New-OSCustomizationSpec -Name $OSSpecName -Type NonPersistent
+
+		Get-OSCustomizationSpec -Name $OSSpecName | Get-OSCustomizationNicMapping | Set-OSCustomizationNicMapping -IpMode UseStaticIP -IpAddress $IPAddress -SubnetMask $SubnetMask -DefaultGateway $DefaultGW -Dns $DNS
+	}
+	
+	Switch ($Version)
+	{
+		2012 { $template = "Windows Server 2012 R2" }
+		2008 { $template = "Windows Server 2008 R2" }
+		Default
+		{
+			Write-Error "Unknown Template. Exiting"
+			Exit
+		}
+	}
+	
+	New-VM -Name $Servername -Template $Template -ResourcePool $ESXiHost -Datastore $Datastore -Notes $Description -OSCustomizationSpec $OSSpecName
+	
+	Set-VM -VM $Servername -NumCpu $CPUCount -MemoryGB $RAMCount -Confirm:$false
+	
+	If ($Dev -eq $true)
+	{
+		Move-VM -VM $Servername -Destination Dev-Servers-VM-Folder -Confirm:$false
+	}
+	
+	Start-VM $Servername
+	Write-Output "Waiting for Customization to Complete."
+	
+	Do
+	{
+	Start-Sleep -Seconds 60
+	$events = Get-VM -Name $servername | Get-VIEvent -Types Info | Where-Object {($_ -is "VMware.Vim.CustomizationSucceeded") -or ($_ -is "VMware.Vim.CustomizationFailed")}
+	Write-Output "Waiting for Customization to Complete."
+	}
+	While ($events -eq $null)
+	
+	Write-Output "Customization Complete."
+
+}
+Finally
+{
+	Disconnect-VIServer -Confirm:$false
+}
