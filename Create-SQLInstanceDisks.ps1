176d885edf8522e1a1c09cefee9a39e799ebe29c
seanpmassey <seanpmassey@gmail.com>
2014-10-10 00:42:13 -0500

    Added Comment based help to scripts

--- Create-SQLInstanceDisks.ps1
+++ Create-SQLInstanceDisks.ps1
@@ -1,4 +1,44 @@
-﻿Param($InstanceName,$DataDriveLetter = "R",$LogDriveLetter = "T", $DataVolumeSize = "20", $LogVolumeSize = "10",$Servername,$vCenter)
+﻿<#
+	.SYNOPSIS
+		A function to create SQL Server instance volumes
+
+	.DESCRIPTION
+		This script will create two disks on a SQL Server for the SQL instance data and log volumes.  These volumes will be configured as mount points and mounted under the Data/Log Volume in a folder named after the instance.  This script is intended to be run on servers that were configured using the Configure-BaseSQLServer.PS1 script.  This script requires Server 2012 R2 or newer.
+
+	.PARAMETER  InstanceName
+		The name of the SQL Server instance that is being installed.  If this will be the default instance, use Default or MSSQLServer as the Instance Name.
+
+	.PARAMETER  Servername
+		The name of the server that is being prepared.
+
+	.PARAMETER  vCenter
+		The name of the vCenter server that manages the VM.
+
+	.PARAMETER CredentialPath
+		Path to the network share where the encrypted passwords are stored.
+	
+	.PARAMETER  DBOwner
+		Optional. The user or group that will owns the SQL Server.  They will be added to the local administrators group.
+
+	.PARAMETER  DataDriveLetter
+		This is the drive letter that SQL Server data volumes will be mounted under.  Defaults to R.
+		
+	.PARAMETER  LogDriveLetter
+		This is the drive letter that SQL Server log volumes will be mounted under.  Defaults to T.
+
+	.PARAMETER  DataVolumeSize
+		The size, in GBs, of the TEMPDB data volume.  Defaults to 10.
+
+	.PARAMETER  LogVolumeSize
+		The size, in GBs, of the TEMPDB log volume.  Defaults to 5.
+	
+	.EXAMPLE
+		PS C:\> Create-SQLInstanceDisks.ps1 -Servername ServerVM1 -InstanceName Test -vcenter vCenter1
+
+#>
+
+
+Param($InstanceName,$DataDriveLetter = "R",$LogDriveLetter = "T", $DataVolumeSize = "20", $LogVolumeSize = "10",$Servername,$vCenter,$CredentialPath="\\PathTo\Credentials")
 
 Function Create-VMDK
 {
